f71ac6de7f37ab14dd4b041fed72f27d3c6ed71b
jodiambra <jodiambra@gmail.com>
2022-10-19 11:29:53 -0400

    adding first histogram

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import plotly_express as px
 from PIL import Image
-from streamlit.commands.page_config import Layout
+from streamlit.commands.page_config import Layout 
 
 #----------------------------#
 # Upgrade S=streamlt library 
@@ -11,22 +11,40 @@ from streamlit.commands.page_config import Layout
 
 #-----------------------------#
 # Page layout 
-st.beta_set_page_config(layout='wide')
+
+st.set_page_config(page_title='US Vehicle Sales : Exploratory Data Analysis', 
+page_icon=None, 
+layout='wide', 
+initial_sidebar_state="auto", 
+menu_items=None)
 #-----------------------------#
 
-# Title
+# Title Picture
+
 image_1 = Image.open('used cars.jpg')
 
 st.image(image_1, width = 1000 )
 
-st.header('US Vehicle Sales')   
-
-st.subheader('Exploratory Data Analysis')
+#-------------------------------#
 
 
+# First look at data table  
 
+st.subheader('Vehicles')
+
+@st.cache
 vehicles = pd.read_csv(
     r'C:/Users/XIX/Documents/coding/projects/Software-Development-Tools/vehicles_us.csv')
+st.checkbox("Use container width", value=False, key="use_container_width")
+st.dataframe(vehicles)
 
+
+#-------------------------------#
+
+# Vehicle Price Histogram
+
+vehicle_price = vehicles['price']
+vehicle_price_fig = px.histogram(vehicle_price, x='price')
+st.write(vehicle_price_fig)
 
 
