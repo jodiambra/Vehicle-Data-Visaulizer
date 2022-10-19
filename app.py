843169e19930b2bde5a8ab53a5abe378e5a9d182
jodiambra <jodiambra@gmail.com>
2022-10-19 09:54:58 -0400

    further updates

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
 #import packages
-import streamlit
+import streamlit as st
 import pandas as pd
-import plotly_express
+import plotly_express as px
+from PIL import Image
+from streamlit.commands.page_config import Layout
+
+#----------------------------#
+# Upgrade S=streamlt library 
+# pip install --upgrade streamlit
+
+#-----------------------------#
+# Page layout 
+st.beta_set_page_config(layout='wide')
+#-----------------------------#
+
+# Title
+image_1 = Image.open('used cars.jpg')
+
+st.image(image_1, width = 1000 )
+
+st.header('US Vehicle Sales')   
+
+st.subheader('Exploratory Data Analysis')
 
 
-vehicles = pd.read_csv(r'C:/Users/XIX/Documents/coding/projects/Software-Development-Tools/vehicles_us.csv')
+
+vehicles = pd.read_csv(
+    r'C:/Users/XIX/Documents/coding/projects/Software-Development-Tools/vehicles_us.csv')
 
 
 
