86391c00394213d553930cbc3e02a4da3e720354
jodiambra <jodiambra@gmail.com>
2022-10-21 09:50:19 -0400

    check upload

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ st.subheader('US Vehicles Data')
 
 vehicles = pd.read_csv(
     r'C:/Users/XIX/Documents/coding/projects/Software-Development-Tools/vehicles_us.csv')
+    
 vehicles['manufacturer'] = vehicles['model'].apply(lambda x: x.split()[0])
 st.dataframe(vehicles)
 
